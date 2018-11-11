baaf9b2a5615427798d57763bfa0a2436156e862
moha96 <moha96@vt.edu>
2018-11-10 21:00:44 -0500

    runs uploaded files

--- service.py
+++ service.py
@@ -1,20 +1,25 @@
-import sys
+import sys, os, socket, subprocess
 from flask import Flask, request, Response
 from functools import wraps
 from AuthDB import DataBase
 from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
-import socket
+from werkzeug.utils import secure_filename
 
-
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() == 'sh'
 
 def find_service(NAME, TYPE):
+    print("Finding address")
     info = zeroconf.get_service_info(TYPE, NAME + '.' + TYPE)
+    print(info)
     addr = socket.inet_ntoa(info.address)
     print(addr)
-    return addr
+    return str(addr)
 
 app = Flask(__name__)
-
+UPLOAD_FOLDER = os.getcwd()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def check_auth(username, password):
     return Auth_db.authenticate(username, password)
 def authenticate():
@@ -41,19 +46,29 @@ def new_user():
 @app.route('/upload/<server_name>', methods=['POST'])
 @requires_auth
 def upload(server_name):
+    
     TYPE = "_http._tcp.local."
-    #browser = ServiceBrowser(self.zeroconf, "_http._tcp.local.", handlers=[on_change])
+
     if (server_name == 'led'):
         NAME = "LED"
     elif (server_name == 'storage'):
         NAME = 'Team 7\'s Storage'
-        addr = find_service(NAME, TYPE)
-
+    print("Got upload command")
+    addr = find_service(NAME, TYPE)
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print("Saved file")
+        subprocess.Popen(["bash", filename, addr])
+    return 'Done'
+        
+        
 if __name__ == "__main__":
     TYPE = "_http._tcp.local."
     #NAME = 'Team 7\'s Storage'
-    NAME = "Team7LED_Rpi"
+    #NAME = "Team7LED_Rpi"
     Auth_db = DataBase()
     zeroconf = Zeroconf()
-    find_service(NAME, TYPE)
+    #find_service(NAME, TYPE)
     app.run(debug=True, host='localhost')
